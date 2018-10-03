1e11b437f81b2e08bc6c2e2b0f5998318452d080
mmayen <mewan074@uottawa.ca>
2018-10-03 18:56:13 -0400

    main: added new functions to Card class
    -All testcases  passing so far

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -3,22 +3,29 @@ package core;
 public class Card {
 	public String suit="";
 	public String rank="";
-;
+	
+	private static final String[] suits = {"C", "D", "H", "S"};
+    private static final String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
-	Card(String suit, String rank){
+	Card(String suit, String rank) {
 		this.suit= suit;
 		this.rank= rank;
 	}
 	
-	public String getSuit(){
+	Card(Integer suit, Integer rank) {
+		this.suit = whatSuit(suit);
+	    this.rank = whatRank(rank);
+	}
+	
+	public String getSuit() {
 		return this.suit;
 	}
 	
-	public String getRank(){
+	public String getRank() {
 		return this.rank;
 	}
 	
-	public boolean equals(Object o){
+	public boolean equals(Object o) {
 		
 		if (!(o instanceof Card)) {
             return false;
@@ -34,5 +41,13 @@ public class Card {
 	        }
 	    }
 		return false;
+	}
+	
+	public String whatSuit(Integer suit){
+		return suits[suit];
+	}
+	
+	public String whatRank(Integer rank){
+		return ranks[rank];
 	}
 }
