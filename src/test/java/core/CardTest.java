bfbe8602c2f69ba550cec4b6e827c5cfb6ae38bf
mmayen <mewan074@uottawa.ca>
2018-10-03 17:42:54 -0400

    refactored: adjusted card testcase

--- src/test/java/core/CardTest.java
+++ src/test/java/core/CardTest.java
@@ -5,19 +5,19 @@ import junit.framework.TestCase;
 public class CardTest extends TestCase {
  
 	public void testGetSuit(){
-		Card card = new Card();
+		Card card = new Card("C","7");
 		
 		assertEquals("C", card.getSuit());
 	}
 	
 	public void testGetRank(){
-		Card card = new Card(); 
+		Card card = new Card("C","7"); 
 		 
 		 assertEquals("7", card.getRank());
 	}
 	
 	public void testEquals() {
-		Card card = new Card(); 
+		Card card = new Card("C","7"); 
 		
 		assertEquals(true, card.equals(new Card("C","7")));	 
     }
