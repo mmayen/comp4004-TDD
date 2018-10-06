f33d4cc5afc71620177b9f03b2bda1df8c7b6a46
mmayen <mewan074@uottawa.ca>
2018-10-06 18:26:42 -0400

    test: test cases for the Hand class

---
+++ src/test/java/core/HandTest.java
@@ -0,0 +1,81 @@
+package core;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+public class HandTest extends TestCase {
+
+	public void testlessHandSize() {
+		 boolean result = false;
+		 try {
+			 ArrayList<Card> lesscards = new ArrayList<Card>();
+			 
+			 for (int x = 0; x < 4; x++){
+				 lesscards.add(new Card("C", "7"));
+				 }
+			 Hand hand = new Hand(lesscards);
+	      }
+		 catch (IllegalArgumentException e) {
+			 result = true;
+		 }
+		 assertTrue(result);
+	}
+	
+	public void testMoreHandSize() {
+		boolean result = false;
+		try {
+			 ArrayList<Card> morecards = new ArrayList<Card>();
+			 
+			 for (int x = 0; x < 6; x++){
+				 morecards.add(new Card("C", "7"));
+				 }
+			Hand hand = new Hand(morecards);
+	        }
+		 catch (IllegalArgumentException e) {
+			 result = true;
+			 }
+	        assertTrue(result);
+	}
+	
+	public void testcalculateHand(){
+		
+	}
+	
+	public void testmakeHandfromFile(){
+	
+	}
+	
+	public void testIsRoyalFlush(){
+
+	}
+	
+	public void testIsStraightFlush(){
+
+	}
+	
+	public void testIsFullhouse(){
+
+	}
+	
+	public void testIsFlush(){
+
+	}
+	
+	public void testIsStraight(){
+
+	}
+	
+	public void testIsFourOfAkind(){
+
+	}
+	
+	public void testIsThreeOfAkind(){
+
+	}
+	
+	public void testIsTwoOfAkind(){
+
+	}
+
+}
